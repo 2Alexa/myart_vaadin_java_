500a23a04e40fe179b3d2b95597e2cc384f0ad2e
Alexandra <alexandra.thomas@live.de>
2021-05-21 11:31:57 +0200

    Initial commit MainView erstellt, MySQL angebunden, Testdaten UserServices zugefügt, CRM erstellt: User,Gefuehl,AbstractEntity Repository erstellt, um Datenbank zuzugreifen, durch API : User,Gefuehl Serviceklasse erstellt: UserService, Gefuehle UI gebaut: MainView , Grid erstellt,spaltenbreite definiert, speichert, gefuhl zu User in UI Spalte definiert Filter methode in MAinView und UserServices eingebunden

--- src/main/java/com/example/application/backend/repository/UserRepository.java
+++ src/main/java/com/example/application/backend/repository/UserRepository.java
@@ -2,8 +2,17 @@ package com.example.application.backend.repository;
 
 import com.example.application.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Long>   {
+    //wenden JPQL Sprache für Abfrage , ob Vorname und Nachname, Zeichenfolge übereinstimmen
+    @Query("select c from User c " +
+            "where lower(c.firstName) like lower(user('%', :searchTerm, '%')) " +
+            "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))")
+    List<User> search(@Param("searchTerm") String searchTerm);
 
 }
--- src/main/java/com/example/application/ui/views/MainView.java
+++ src/main/java/com/example/application/ui/views/MainView.java
@@ -7,21 +7,27 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import java.awt.*;
+
 @Route("")
 public class MainView extends VerticalLayout {
+
     private UserServices userServices;
     //neues Fensterraster definieren
     private Grid<User> grid = new Grid<>(User.class);
+    //Textfeld erstellt
+    private TextField filterText = new TextField();
+
+
 
     public MainView(UserServices userServices) {
-        //speicher UserService in Field ,zu Zugriff
         this.userServices = userServices;
-        addClassName("list-view");// gleich klass wie css
-        setSizeFull();//volle groesser browserfenster
+        addClassName("list-view");
+        setSizeFull();
         configureGrid();
 
-        add(grid);//füge zu hauptview zu
-        updateList();//methode, um alle Kontakte anzurufen
+        add(grid);
+        updateList();
     }
 
     private void updateList() {
@@ -34,10 +40,14 @@ public class MainView extends VerticalLayout {
         grid.removeColumnByKey("gefuehl");//entfernt Standartspaltendefi
         grid.setColumns("firstName", "lastName", "email", "status");//defi eigenschaft des users
         grid.addColumn(user -> {//zeigt den user Paramenter auf, und den gefuehl namen oder - wenn leer
-            Gefuehl gefuehl =user.getGefuehl();
+            Gefuehl gefuehl = user.getGefuehl();
             return gefuehl == null ? "-" : gefuehl.getName();
         }).setHeader("Gefuehl");//defi spaltenüberschrift
-        grid.getColumns().forEach(col->col.setAutoWidth(true));// aktiviert automatische spaltenbreite
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));// aktiviert automatische spaltenbreite
 
     }
 }
+
+
+
+
