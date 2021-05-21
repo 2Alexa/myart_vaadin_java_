d03fd77ccf89086199ee4061be1fa476b1716a7f
Alexandra <alexandra.thomas@live.de>
2021-05-21 10:26:44 +0200

    Initial commit MainView erstellt, MySQL angebunden, Testdaten UserServices zugefügt, CRM erstellt: User,Gefuehl,AbstractEntity Repository erstellt, um Datenbank zuzugreifen, durch API : User,Gefuehl Serviceklasse erstellt: UserService, Gefuehle UI gebaut: MainView , Grid erstellt,spaltenbreite definiert, speichert, gefuhl zu User in UI Spalte definiert

---
+++ src/main/java/com/example/application/backend/repository/GefuehlRepository.java
@@ -0,0 +1,9 @@
+package com.example.application.backend.repository;
+
+
+import com.example.application.backend.entity.Gefuehl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GefuehlRepository extends JpaRepository<Gefuehl, Long> {
+
+}
---
+++ src/main/java/com/example/application/ui/views/dashboard/dashboard.java
@@ -0,0 +1,11 @@
+package com.example.application.ui.views.dashboard;
+
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class dashboard extends VerticalLayout {
+    public dashboard(){
+
+    }
+
+}
